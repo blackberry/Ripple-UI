cbe6b279e794ac6adbb97082762247f01fd3f971
brent lintner <brent.lintner@gmail.com>
2011-07-28 15:24:09 -0400

    added W3C geolocation to mobile web platform

---
+++ test/web/geolocation.js
@@ -0,0 +1,31 @@
+describe("w3c_geolocation", function () {
+    var w3c = "w3c/1.0/",
+        web = "ripple/platform/web/default/",
+        spec = require(web  + 'spec'),
+        ui = require(web  + 'spec/ui');
+
+    describe("spec", function () {
+        it("includes require modules", function () {
+            expect(spec.objects.Coordinates.path).toEqual(w3c + "Coordinates");
+            expect(spec.objects.Position.path).toEqual(w3c + "Position");
+            expect(spec.objects.PositionError.path).toEqual(w3c + "PositionError");
+            expect(spec.objects.navigator.children.geolocation.path).toEqual(w3c + "geolocation");
+        });
+
+        describe("ui", function () {
+            it("uses web/spec/ui module", function () {
+                expect(spec.ui.plugins).toEqual(ui.plugins);
+            });
+
+            it("includes geoView plugin", function () {
+                function includesGeoViewPlugin() {
+                    return ui.plugins.some(function (plugin) {
+                        return plugin === "geoView";
+                    });
+                }
+
+                expect(includesGeoViewPlugin()).toBe(true);
+            });
+        });
+    });
+});
