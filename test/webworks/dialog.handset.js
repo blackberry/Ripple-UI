9d193a732372f0e458071c1fa936f8409d4bfec7
Gord Tanner (macbook) <gord@tinyhippos.com>
2011-08-04 09:33:20 -0400

    added some tests for webworks handset dialog

---
+++ test/webworks/dialog.handset.js
@@ -0,0 +1,33 @@
+/*
+ *  Copyright 2011 Research In Motion Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+describe("webworks dialog", function () {
+    var dialogClient = require('ripple/platform/webworks/2.0.0/client/dialog');
+
+    describe("client", function () {
+        describe("customAsk", function () {
+            it("throws an exception", function () {
+                expect(dialogClient.customAsk).toThrow("not implemented");
+            });
+        });
+
+        describe("standardAsk", function () {
+
+            it("throws an exception", function () {
+                expect(dialogClient.standardAsk).toThrow("not implemented");
+            });
+        });
+    });
+});
