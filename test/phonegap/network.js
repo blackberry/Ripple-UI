0b1330b1251505915f82d5c20242d9914d237607
Gord Tanner <gtanner@gmail.com>
2011-08-08 16:32:29 -0400

    updated phonegap network api to latest version

--- test/phonegap/network.js
+++ test/phonegap/network.js
@@ -14,89 +14,11 @@
  * limitations under the License.
  */
 describe("phonegap_network", function () {
-    var s, _oldRetrieve,
-        sinon = require('sinon'),
-        deviceSettings = require('ripple/deviceSettings'),
-        NetworkStatus = require('ripple/platform/phonegap/0.9/NetworkStatus'),
-        network = require('ripple/platform/phonegap/0.9/network'),
-        _networkConnection = true,
-        _network = "GPRS";
+    var deviceSettings = require('ripple/deviceSettings'),
+        network = require('ripple/platform/phonegap/0.9/network');
 
-    beforeEach(function () {
-        _oldRetrieve = deviceSettings.retrieve;
-        deviceSettings.retrieve = function (key) {
-            switch (key) {
-            case "NetworkStatus.isDataNetworkConnected":
-                return _networkConnection;
-            case "NetworkStatus.networkConnectionType":
-                if (_networkConnection) {
-                    return _network;
-                }
-                else {
-                    return _networkConnection;
-                }
-            }
-        };
-
-        s = sinon.create(sinon.sandbox);
-    });
-
-    afterEach(function () {
-        s.verifyAndRestore();
-        deviceSettings.retrieve = _oldRetrieve;
-        _network = "GPRS";
-        _networkConnection = true;
-    });
-
-    describe("isReachable", function () {
-        it("calls the callback", function () {
-            network.isReachable("foo", s.mock().once());
-        });
-
-        it("can be called without a callback", function () {
-            expect(function () {
-                network.isReachable("foo");
-            }).not.toThrow();
-        });
-
-        it("throws exception when callback is not a function", function () {
-            expect(function () {
-                network.isReachable("foo", "bar");
-            }).toThrow();
-        });
-
-        it("thows exception when no host is specified", function () {
-            expect(function () {
-                network.isReachable();
-            }).toThrow();
-        });
-
-        it("calls callback with REACHABLE_VIA_CARRIER_DATA_NETWORK by default", function () {
-            network.isReachable("google.com", s.mock().once().withExactArgs(NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK));
-        });
-
-        it("calls callback with NOT_REACHABLE when network status is REACHABLE_VIA_WIFI_NETWORK isDataNetworkConnected is false", function () {
-            _networkConnection = false;
-            _network = "WIFI";
-            network.isReachable("google.com", s.mock().once().withExactArgs(NetworkStatus.NOT_REACHABLE));
-        });
-
-        it("calls callback with NOT_REACHABLE when network status is REACHABLE_VIA_CARRIER_DATA_NETWORK and isDataNetworkConnected is false", function () {
-            _networkConnection = false;
-            _network = "GPRS";
-            network.isReachable("google.com", s.mock().once().withExactArgs(NetworkStatus.NOT_REACHABLE));
-        });
-
-        it("calls callback with REACHABLE_VIA_CARRIER_DATA_NETWORK when isDataNetworkConnected is true and networkConnectionType is GPRS", function () {
-            _networkConnection = true;
-            _network = "GPRS";
-            network.isReachable("google.com", s.mock().once().withExactArgs(NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK));
-        });
-
-        it("calls callback with REACHABLE_VIA_WIFI_NETWORK when isDataNetworkConnected is true and networkConnectionType is WIFI", function () {
-            _networkConnection = true;
-            _network = "WIFI";
-            network.isReachable("google.com", s.mock().once().withExactArgs(NetworkStatus.REACHABLE_VIA_WIFI_NETWORK));
-        });
+    it("returns the value from deviceSettings", function () {
+        spyOn(deviceSettings, "retrieve").andReturn("tin_cans");
+        expect(network.connection.type).toBe("tin_cans");
     });
 });
