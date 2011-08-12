c45cc7a67df9c990d7617ce6e4487461501780b7
Nino D'Aversa <nino@ndaversa.com>
2011-08-12 14:51:31 -0400

    Adding licence header and unit tests for devicemotion and deviceorientation.

--- test/unit/accelerometer.js
+++ test/unit/accelerometer.js
@@ -16,6 +16,8 @@
 describe("accelerometer", function () {
 
     var accelerometer = require('ripple/accelerometer'),
+        Rotation = require('ripple/platform/w3c/1.0/Rotation'),
+        Acceleration = require('ripple/platform/w3c/1.0/Acceleration'),
         event = require('ripple/event'),
         sinon = require('sinon'),
         s,
@@ -23,7 +25,10 @@ describe("accelerometer", function () {
         MOCK_POSITIONINFO = {
             x: 4,
             y: 5,
-            z: -9.8
+            z: -9.8,
+            alpha: 30,
+            beta: 45,
+            gamma: 90
         };
 
     beforeEach(function () {
@@ -67,6 +72,72 @@ describe("accelerometer", function () {
         expect(MOCK_POSITIONINFO.x).toBe(info.accelerationIncludingGravity.x);
         expect(MOCK_POSITIONINFO.y).toBe(info.accelerationIncludingGravity.y);
         expect(MOCK_POSITIONINFO.z).toBe(info.accelerationIncludingGravity.z);
+    });
+
+    it("should fire DeviceMotionEvent when setInfo is called", function () {
+        s.stub(db, "saveObject");
+        spyOn(event, "trigger");
+
+        accelerometer.setInfo({
+            x: MOCK_POSITIONINFO.x,
+            y: MOCK_POSITIONINFO.y,
+            z: MOCK_POSITIONINFO.z,
+            alpha: MOCK_POSITIONINFO.alpha,
+            beta: MOCK_POSITIONINFO.beta,
+            gamma: MOCK_POSITIONINFO.gamma
+        });
+
+        expect(event.trigger).toHaveBeenCalledWith("DeviceMotionEvent", [{
+                acceleration: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y, MOCK_POSITIONINFO.z),
+                accelerationIncludingGravity: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y , MOCK_POSITIONINFO.z),
+                rotationRate: new Rotation(0, 0, 0),
+                orientation: new Rotation(MOCK_POSITIONINFO.alpha, MOCK_POSITIONINFO.beta, MOCK_POSITIONINFO.gamma),
+                timestamp: new Date().getTime()
+        }]);
+    });
+
+    it("should fire DeviceMotionEvent when setInfo is called", function () {
+        s.stub(db, "saveObject");
+        spyOn(event, "trigger");
+
+        accelerometer.setInfo({
+            x: MOCK_POSITIONINFO.x,
+            y: MOCK_POSITIONINFO.y,
+            z: MOCK_POSITIONINFO.z,
+            alpha: MOCK_POSITIONINFO.alpha,
+            beta: MOCK_POSITIONINFO.beta,
+            gamma: MOCK_POSITIONINFO.gamma
+        });
+
+        expect(event.trigger).toHaveBeenCalledWith("DeviceMotionEvent", [{
+                acceleration: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y, MOCK_POSITIONINFO.z),
+                accelerationIncludingGravity: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y , MOCK_POSITIONINFO.z),
+                rotationRate: new Rotation(0, 0, 0),
+                orientation: new Rotation(MOCK_POSITIONINFO.alpha, MOCK_POSITIONINFO.beta, MOCK_POSITIONINFO.gamma),
+                timestamp: new Date().getTime()
+        }]);
+    });
+
+    it("should fire DeviceOrientationEvent when setInfo is called", function () {
+        s.stub(db, "saveObject");
+        spyOn(event, "trigger");
+
+        accelerometer.setInfo({
+            x: MOCK_POSITIONINFO.x,
+            y: MOCK_POSITIONINFO.y,
+            z: MOCK_POSITIONINFO.z,
+            alpha: MOCK_POSITIONINFO.alpha,
+            beta: MOCK_POSITIONINFO.beta,
+            gamma: MOCK_POSITIONINFO.gamma
+        });
+
+        expect(event.trigger).toHaveBeenCalledWith("DeviceOrientationEvent", [{
+                acceleration: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y, MOCK_POSITIONINFO.z),
+                accelerationIncludingGravity: new Acceleration(MOCK_POSITIONINFO.x, MOCK_POSITIONINFO.y , MOCK_POSITIONINFO.z),
+                rotationRate: new Rotation(0, 0, 0),
+                orientation: new Rotation(MOCK_POSITIONINFO.alpha, MOCK_POSITIONINFO.beta, MOCK_POSITIONINFO.gamma),
+                timestamp: new Date().getTime()
+        }]);
     });
 
     it("test should return a valid cached object when specified", function () {
