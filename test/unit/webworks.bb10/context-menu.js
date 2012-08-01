3b2664d9579ccf61e1b9a60cbd1eb1444bde6332
Stephan Leroux <stephanleroux@gmail.com>
2012-08-01 16:48:17 -0400

    Added unit tests for addItem / removeItem from context menu

--- test/unit/webworks.bb10/context-menu.js
+++ test/unit/webworks.bb10/context-menu.js
@@ -16,65 +16,129 @@
 describe("context menu", function () {
     var ui = require('ripple/ui'),
         contextMenu,
-        events = require('ripple/event');
-    
+        events = require('ripple/event'),
+        goodAction = { 
+            actionId: 'MyItem', 
+            label: 'My Item', 
+            icon: 'http://mysite.com/icon.png' 
+        }, 
+        actionNoId = {
+            label: 'My Item', 
+            icon: 'http://mysite.com/icon.png' 
+        }, 
+        actionInvalidId = {
+            actionId: '', 
+            label: 'My Item', 
+            icon: 'http://mysite.com/icon.png' 
+        },
+        customMenuCallback;
+
     beforeEach(function () {
         spyOn(ui, 'showOverlay');
         contextMenu = require('ripple/platform/webworks.bb10/1.0.0/context-menu');
+        customMenuCallback = jasmine.createSpy('customMenuCallback');
     });
 
-    describe("existentials", function () {
-        it("should exist ", function () {
-            expect(typeof contextMenu).not.toBe('undefined' || null);
-        });
-        
-        it("should have an enabled property", function () {
-            expect(contextMenu.enabled).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_ALL", function () {
-            expect(contextMenu.CONTEXT_ALL).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_LINK", function () {
-            expect(contextMenu.CONTEXT_LINK).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_IMAGE_LINK", function () {
-            expect(contextMenu.CONTEXT_IMAGE_LINK).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_IMAGE", function () {
-            expect(contextMenu.CONTEXT_IMAGE).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_INPUT", function () {
-            expect(contextMenu.CONTEXT_INPUT).toBeDefined();
-        });
-
-        it("should have a readonly field named CONTEXT_TEXT", function () {
-            expect(contextMenu.CONTEXT_TEXT).toBeDefined();
-        });
+    it("exists", function () {
+        expect(contextMenu).toBeDefined();
     });
 
-    describe("mutations", function () {
-        it("should allow the enabled property to be set", function () {
-            contextMenu.enabled = false;
-            expect(contextMenu.enabled).toBeFalsy();
-            contextMenu.enabled = true;
-            expect(contextMenu.enabled).toBeTruthy();
-        });
+    // Make sure API signature is implemented correctly
+    it("has an enabled property", function () {
+        expect(contextMenu.enabled).toBeDefined();
     });
 
-    describe("events", function () {
-        it("should receive ContextMenuEvent event", function () {
-            events.trigger('ContextMenuEvent', ['context'], true);
-            expect(ui.showOverlay).toHaveBeenCalled();
-        });
+    it("has a readonly field named CONTEXT_ALL", function () {
+        expect(contextMenu.CONTEXT_ALL).toBeDefined();
+    });
 
-        it("should received LayoutChanged event", function () {
-            events.trigger('LayoutChanged', [], true);
-            expect(ui.showOverlay).toHaveBeenCalled();
-        });
+    it("has have a readonly field named CONTEXT_LINK", function () {
+        expect(contextMenu.CONTEXT_LINK).toBeDefined();
+    });
+
+    it("has a readonly field named CONTEXT_IMAGE_LINK", function () {
+        expect(contextMenu.CONTEXT_IMAGE_LINK).toBeDefined();
+    });
+
+    it("has a readonly field named CONTEXT_IMAGE", function () {
+        expect(contextMenu.CONTEXT_IMAGE).toBeDefined();
+    });
+
+    it("has a readonly field named CONTEXT_INPUT", function () {
+        expect(contextMenu.CONTEXT_INPUT).toBeDefined();
+    });
+
+    it("has a readonly field named CONTEXT_TEXT", function () {
+        expect(contextMenu.CONTEXT_TEXT).toBeDefined();
+    });
+
+    it("has addItem field defined", function () {
+        expect(contextMenu.addItem).toBeDefined();
+    });
+
+    it("has removeItem field defined", function () {
+        expect(contextMenu.removeItem).toBeDefined();
+    });
+
+    it("allows the enabled property to be set", function () {
+        contextMenu.enabled = false;
+        expect(contextMenu.enabled).toBeFalsy();
+        contextMenu.enabled = true;
+        expect(contextMenu.enabled).toBeTruthy();
+    });
+
+    it("receives ContextMenuEvent event", function () {
+        events.trigger('ContextMenuEvent', [contextMenu.CONTEXT_ALL], true);
+        expect(ui.showOverlay).toHaveBeenCalled();
+    });
+
+    it("receives LayoutChanged event", function () {
+        events.trigger('LayoutChanged', [], true);
+        expect(ui.showOverlay).toHaveBeenCalled();
+    });
+
+    it("can have custom items added and removed", function () {
+        contextMenu.addItem([contextMenu.CONTEXT_ALL], goodAction, customMenuCallback);
+        contextMenu.removeItem([contextMenu.CONTEXT_ALL], goodAction.actionId);
+    });
+
+    it("supports firing custom menu events", function () {        
+        contextMenu.addItem([contextMenu.CONTEXT_ALL], goodAction, customMenuCallback);
+        events.trigger('contextmenu.executeMenuAction', [goodAction.actionId], true);
+        expect(customMenuCallback).toHaveBeenCalled();
+
+        contextMenu.removeItem([contextMenu.CONTEXT_ALL], goodAction.actionId);
+    });
+
+    it("throws an error when adding duplicate custom menu items", function () {
+        contextMenu.addItem([contextMenu.CONTEXT_ALL], goodAction, customMenuCallback);
+
+        expect(function () {
+            contextMenu.addItem([contextMenu.CONTEXT_ALL], goodAction, customMenuCallback);
+        }).toThrow();
+
+        contextMenu.removeItem([contextMenu.CONTEXT_ALL], goodAction.actionId);
+    });
+
+    it("throws an error when adding custom menu item with actionId = '' ", function () {
+        expect(function () {
+            contextMenu.addItem([contextMenu.CONTEXT_ALL], actionInvalidId, customMenuCallback);
+        }).toThrow();
+    });
+
+    it("throws an error when adding custom menu item with no actionId", function () {
+        expect(function () {
+            contextMenu.addItem([contextMenu.CONTEXT_ALL], actionNoId, customMenuCallback);
+        }).toThrow();
+    });
+
+    it("doesn't invoke callback for a removed custom menu item", function () {
+        contextMenu.addItem([contextMenu.CONTEXT_ALL], goodAction, customMenuCallback);
+        events.trigger('contextmenu.executeMenuAction', [goodAction.actionId], true);
+        expect(customMenuCallback).wasCalled();
+
+        contextMenu.removeItem([contextMenu.CONTEXT_ALL], goodAction.actionId);
+        events.trigger('contextmenu.executeMenuAction', [goodAction.actionId], true);
+        expect(customMenuCallback.callCount).toEqual(1);
     });
 });
