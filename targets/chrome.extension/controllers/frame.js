07d0090afe8ed4a4294fb531e54bbc62e084af06
Brent Lintner <brent.lintner@gmail.com>
2012-12-06 11:42:07 -0500

    The Chrome Extension iframe injection should check for `ripple`.
    Previously, it was checking for `window.top.require`, then using
    `window.top.ripple` to require modules. This was an oversight done in a
    previous refactor that did not break things until all the code in
    Ripple's client file (except `window.ripple`) was closured and hidden
    from the global scope.

--- targets/chrome.extension/controllers/frame.js
+++ targets/chrome.extension/controllers/frame.js
@@ -16,6 +16,6 @@
 if (!document.getElementById("emulator-booting") && !document.getElementById("tinyhippos-injected")) {
     var script = document.createElement("script");
     script.id = "tinyhippos-injected";
-    script.innerText = 'if (window.top.require) { window.top.ripple("bootstrap").inject(window, document); }'; 
+    script.innerText = 'if (window.top.ripple) { window.top.ripple("bootstrap").inject(window, document); }';
     document.documentElement.appendChild(script);
 }
