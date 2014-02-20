e84f67d51c44ad7d35f8bb659ee9ed3222c1dbd8
Ken <kwallis@rim.com>
2014-02-20 12:32:18 -0800

    removing contents of master as project has moved to Apache. Leaving README in place to point to Apache

--- thirdparty/ludicrous.js
+++
@@ -1,4 +0,0 @@
-console.log("Prepare ship for ludicrous speed!");
-console.log();
-console.log("Fasten all seatbelts, seal all entrances and exits, close all shops in the mall, cancel the three ring circus, secure all animals in the zoo!");
-console.log();
